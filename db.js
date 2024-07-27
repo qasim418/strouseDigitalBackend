b1365658c3fa9f51aed5e329917c802a34224c48
Muhammad Qasim <qasimvirk90@gmail.com>
2024-07-28 04:00:32 +0500

    chore: Update database configuration to use environment variables

--- db.js
+++ db.js
@@ -1,15 +1,35 @@
-// backend/db.js
+// // backend/db.js
+// const mysql = require('mysql');
+
+// const connection = mysql.createConnection({
+//   host: 'localhost',
+//   user: 'root', // use your MySQL username
+//   password: '', // use your MySQL password
+//   database: 'usaapp',
+// });
+
+// connection.connect((err) => {
+//   if (err) throw err;
+//   console.log('Connected to MySQL');
+// });
+
+// module.exports = connection;
 const mysql = require('mysql');
+require('dotenv').config();
 
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root', // use your MySQL username
-  password: '', // use your MySQL password
-  database: 'usaapp',
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
 connection.connect((err) => {
-  if (err) throw err;
+  if (err) {
+    console.error('Database connection error:', err);
+    throw err;
+  }
   console.log('Connected to MySQL');
 });
 
